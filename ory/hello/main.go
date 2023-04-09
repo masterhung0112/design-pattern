a04d61c1cfb44efbcacf5dcc6a56a932b6e17662
masterhung0112 <masterhung0112@gmail.com>
2023-04-09 17:27:38 +0900

    Add the ong

---
+++ ory/hello/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "hello"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8090", nil))
+}
